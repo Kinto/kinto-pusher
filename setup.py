3c91870a66d0c836cc101c1c934304ccc606805e
Rémy HUBSCHER <remy@chefclub.tv>
2019-05-22 16:15:57 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,54 +1,50 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+import os
+import codecs
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
 
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+def read_file(filename):
+    """Open a related file and return its content."""
+    with codecs.open(os.path.join(here, filename), encoding="utf-8") as f:
+        content = f.read()
+    return content
 
 
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
+README = read_file("README.rst")
+CHANGELOG = read_file("CHANGELOG.rst")
 
-with open('CHANGELOG.rst') as history_file:
-    history = history_file.read()
-
-requirements = [
-    'kinto>=3.0.0',
-    'pusher'
-]
+requirements = ["kinto", "pusher"]
 
 test_requirements = [
     # TODO: put package test requirements here
 ]
 
 setup(
-    name='kinto-pusher',
-    version='0.7.0.dev0',
+    name="kinto-pusher",
+    version="0.7.0.dev0",
     description="Plug Kinto notifications into Pusher.com",
-    long_description=readme + '\n\n' + history,
+    long_description=README + "\n\n" + CHANGELOG,
     author="Mathieu Leplatre",
-    author_email='mathieu@leplat.re',
-    url='https://github.com/Kinto/kinto-pusher',
-    packages=[
-        'kinto_pusher',
-    ],
-    package_dir={'kinto_pusher':
-                 'kinto_pusher'},
+    author_email="mathieu@leplat.re",
+    url="https://github.com/Kinto/kinto-pusher",
+    packages=["kinto_pusher"],
+    package_dir={"kinto_pusher": "kinto_pusher"},
     include_package_data=True,
     install_requires=requirements,
     license="Apache License (2.0)",
     zip_safe=False,
-    keywords='kinto pusher',
+    keywords="kinto pusher",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
-    test_suite='tests',
-    tests_require=test_requirements
+    test_suite="tests",
+    tests_require=test_requirements,
 )
